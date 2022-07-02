666119e24dd3dbd596146b7732386b64e02a8381
Tomislav Kusevic <tomislav.kusevic@codecons.com>
2022-07-03 01:04:35 +0200

    updated typeormConfig.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,54 +1,58 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import {
   TypeOrmModuleAsyncOptions,
   TypeOrmModuleOptions,
-} from '@nestjs/typeorm';
+} from "@nestjs/typeorm";
 
 export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
   inject: [ConfigService],
   useFactory: async (): Promise<TypeOrmModuleOptions> => {
     return {
-      type: 'postgres',
-      host: process.env.DATABASE_URL,
+      type: "postgres",
+      host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       database: process.env.DB_NAME,
       password: process.env.DB_PASSWORD,
-      entities: [__dirname + '/../**/*.entity.{js,ts}'],
-      migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
+      entities: [__dirname + "/../**/*.entity.{js,ts}"],
+      migrations: [__dirname + "/../database/migrations/*{.ts,.js}"],
       keepConnectionAlive: true,
       cli: {
-        migrationsDir: __dirname + '/../database/migrations',
+        migrationsDir: __dirname + "/../database/migrations",
       },
       extra: {
-        charset: 'utf8mb4_unicode_ci',
+        charset: "utf8mb4_unicode_ci",
       },
       synchronize: false,
       url: process.env.DATABASE_URL,
-      ssl: process.env.DATABASE_URL ? true : false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
       logging: true,
     };
   },
 };
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.DATABASE_URL,
+  type: "postgres",
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
+  entities: [__dirname + "/../**/*.entity.{js,ts}"],
+  migrations: [__dirname + "/../database/migrations/*{.ts,.js}"],
   cli: {
-    migrationsDir: __dirname + '/../database/migrations',
+    migrationsDir: __dirname + "/../database/migrations",
   },
   extra: {
-    charset: 'utf8mb4_unicode_ci',
+    charset: "utf8mb4_unicode_ci",
   },
   synchronize: false,
   url: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? true : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   logging: true,
 };
